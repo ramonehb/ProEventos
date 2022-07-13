d3aaa161ae97bbceacebf31e8abe90a830e6a313
Humberto Ramone <humbertorody@gmail.com>
2022-07-13 19:29:06 -0300

    Camada de Serviço finalizada

--- Server/src/ProEventos.Service/EventoService.cs
+++ Server/src/ProEventos.Service/EventoService.cs
@@ -1,39 +1,120 @@
 ﻿using System;
 using System.Threading.Tasks;
 using ProEventos.Domain;
+using ProEventos.Persistence.Interfaces;
 
-namespace ProEventos.Application
+namespace ProEventos.Service
 {
     public class EventoService : IEventoService
     {
-        public Task<Evento> AddEventos(Evento model)
+        private readonly IGeralPersist _geralPersist;
+        private readonly IEventoPersist _eventoPersist;
+        public EventoService(IGeralPersist geralPersist, IEventoPersist eventoPersist)
         {
-            throw new NotImplementedException();
+            _geralPersist = geralPersist;
+            _eventoPersist = eventoPersist;
         }
 
-        public Task<Evento> DeleteEvento(int id)
+        public async Task<Evento> AddEventos(Evento model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _geralPersist.Add<Evento>(model);
+                if (await _geralPersist.SaveChangeAsync())
+                {
+                    return await _eventoPersist.GetEventoByIdAsync(model.Id, false);
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Erro: {e.Message}");
+            }
         }
 
-        public Task<Evento[]> GetAllEventosAsync(bool includePalestrantes)
+        public async Task<Evento> UpdateEvento(int id, Evento model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var evento = await _eventoPersist.GetEventoByIdAsync(id);
+                if (evento is null) return null;
+
+                model.Id = evento.Id;
+                _geralPersist.Update(model);
+                if (await _geralPersist.SaveChangeAsync())
+                {
+                    return await _eventoPersist.GetEventoByIdAsync(model.Id, false);
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Erro: {e.Message}");
+            }
         }
 
-        public Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes)
+        public async Task<bool> DeleteEvento(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var evento = await _eventoPersist.GetEventoByIdAsync(id);
+                if (evento is null) throw new Exception("Evento para delete não encontrado.");
+
+                _geralPersist.Delete<Evento>(evento);
+
+                return await _geralPersist.SaveChangeAsync();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Erro: {e.Message}");
+            }
         }
 
-        public Task<Evento> GetEventoByIdAsync(int id, bool includePalestrantes)
+        public async Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var eventos = await _eventoPersist.GetAllEventosAsync(includePalestrantes);
+                if (eventos is null) return null;
+
+                return eventos;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Erro: {e.Message}");
+            }
         }
 
-        public Task<Evento> Update(int id, Evento model)
+        public async Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var eventos = await _eventoPersist.GetAllEventosByTemaAsync(tema);
+                if (eventos is null) return null;
+
+                return eventos;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Erro: {e.Message}");
+            }
+        }
+
+        public async Task<Evento> GetEventoByIdAsync(int id, bool includePalestrantes = false)
+        {
+            try
+            {
+                var eventos = await _eventoPersist.GetEventoByIdAsync(id);
+                if (eventos is null) return null;
+
+                return eventos;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Erro: {e.Message}");
+            }
         }
     }
 }
--- Server/src/ProEventos.Service/Interfaces/IEventoService.cs
+++ Server/src/ProEventos.Service/Interfaces/IEventoService.cs
@@ -1,16 +1,16 @@
 using System.Threading.Tasks;
 using ProEventos.Domain;
 
-namespace ProEventos.Application
+namespace ProEventos.Service
 {
     public interface IEventoService
     {
         Task<Evento> AddEventos(Evento model);
-        Task<Evento> Update(int id, Evento model);
-        Task<Evento> DeleteEvento(int id);
+        Task<Evento> UpdateEvento(int id, Evento model);
+        Task<bool> DeleteEvento(int id);
 
-        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
-        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
-        Task<Evento> GetEventoByIdAsync(int id, bool includePalestrantes);
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes = false);
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes = false);
+        Task<Evento> GetEventoByIdAsync(int id, bool includePalestrantes = false);
     }
 }
