673f4d0be88f6cf90337ec89427d1bad181cc422
Humberto Ramone <humbertorody@gmail.com>
2022-07-15 00:29:50 -0300

    feat:     Incluindo do ngx-bootstrap Tooltip nos botoes editar/excluir e incluindo Dropdown na navbar

--- Client/ProEventos-App/src/app/app.module.ts
+++ Client/ProEventos-App/src/app/app.module.ts
@@ -9,10 +9,12 @@ import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { EventoService } from './services/evento.service';
-
 import { NavComponent } from './nav/nav.component';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 
 @NgModule({
@@ -29,7 +31,9 @@ import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
-    FormsModule
+    FormsModule,
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot()
   ],
   providers: [
     EventoService
