566f976c6feaa233c0e320cad2dd6d3547e10e08
Humberto Ramone <humbertorody@gmail.com>
2022-07-17 14:09:24 -0300

    refactor:  Adicionando bootstrap no componente Titulo e incluindo no projeto o bootswatch

--- Client/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Client/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -7,6 +7,9 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TituloComponent implements OnInit {
   @Input() titulo = '';
+  @Input() subtitulo = 'Desde 2022';
+  @Input() iconClass = 'fa fa-user';
+  @Input() botaoListar = false;
 
   constructor() { }
 
