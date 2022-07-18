826845f4cec73c183d7f4e1708a010b7ca2f9188
Humberto Ramone <humbertorody@gmail.com>
2022-07-18 18:57:17 -0300

    chore:  Incluindo configuracao para melhorar importacoes

--- Client/ProEventos-App/src/app/componentes/eventos/evento-lista/evento-lista.component.ts
+++ Client/ProEventos-App/src/app/componentes/eventos/evento-lista/evento-lista.component.ts
@@ -3,8 +3,8 @@ import { Router } from '@angular/router';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
-import { Evento } from 'src/app/models/Evento';
-import { EventoService } from 'src/app/services/evento.service';
+import { Evento } from '@app/models/Evento';
+import { EventoService } from '@app/services/evento.service';
 
 @Component({
   selector: 'app-evento-lista',
