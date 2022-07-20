8ac49b788bc8d9f79b7a5b61ef58e50c51d16576
Humberto Ramone <humbertorody@gmail.com>
2022-07-19 21:36:30 -0300

    feat: Validacao do formulario de perfil

--- Client/ProEventos-App/src/app/componentes/user/perfil/perfil.component.ts
+++ Client/ProEventos-App/src/app/componentes/user/perfil/perfil.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.scss']
 })
 export class PerfilComponent implements OnInit {
+  form: FormGroup = this.formBuilder.group({});
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private toatrs: ToastrService
+  ) { }
 
   public ngOnInit(): void {
+    this.validationForm();
   }
 
+  get f(): any{
+    return this.form.controls;
+  }
+
+  public validationForm(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.ValidaSenhas('password', 'confirmPass')
+    };
+
+    this.form = this.formBuilder.group({
+      // titulo: ['', Validators.required],
+      nome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      ultimoNome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(20)]],
+      // funcao: ['', Validators.required],
+      descricao: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]],
+      confirmPass: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]]
+    }, formOptions);
+  }
+
+  public confirm(): void {
+    this.toatrs.success('Perfil cadastrado com sucesso', 'Atenção');
+  }
 }
