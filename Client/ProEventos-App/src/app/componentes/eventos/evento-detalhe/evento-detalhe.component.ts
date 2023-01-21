dffef88aa008cef500b2259cc20d7193f0f97f38
Humberto Ramone <humbertorody@gmail.com>
2023-01-20 22:24:54 -0300

    feat: inlcusao e exclusao do lote, e validao no front da pagina de login

--- Client/ProEventos-App/src/app/componentes/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Client/ProEventos-App/src/app/componentes/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -4,8 +4,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Evento } from '@app/models/Evento';
 import { Lote } from '@app/models/Lote';
 import { EventoService } from '@app/services/evento.service';
+import { LoteService } from '@app/services/lote.service';
 import { Constants } from '@app/util/constants';
 import { BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 
@@ -17,9 +19,13 @@ import { ToastrService } from 'ngx-toastr';
 export class EventoDetalheComponent implements OnInit {
 
   evento = {} as Evento;
+  eventoId: number;
   metodo = 'post';
   novo = true;
+  novoLote = true;
   form: FormGroup = this.formBuilder.group({});
+  modalRef: BsModalRef;
+  loteAtual = {id: 0, nome: '', indice: 0};
 
   constructor(
     private formBuilder: FormBuilder,
@@ -28,7 +34,9 @@ export class EventoDetalheComponent implements OnInit {
     private routerActive: ActivatedRoute,
     private eventoService: EventoService,
     private spinner: NgxSpinnerService,
-    private router: Router
+    private router: Router,
+    private loteService: LoteService,
+    private modalService: BsModalService
   ) {
     this.localeService.use(Constants.LocalProject);
   }
@@ -58,6 +66,15 @@ export class EventoDetalheComponent implements OnInit {
     };
   }
 
+  get bsConfigLote(): any {
+    return {
+      adaptivePosition: true,
+      dateInputFormat: 'DD/MM/YYYY',
+      containerClass: 'theme-default',
+      showWeekNumbers: false,
+    };
+  }
+
   public adicionarLote(): void {
     this.lotes.push(this.criarLote({id: 0} as Lote));
   }
@@ -74,17 +91,21 @@ export class EventoDetalheComponent implements OnInit {
   }
 
   public carregaEvento(): void {
-    const eventoId = this.routerActive.snapshot.paramMap.get('id');
+    this.eventoId = +this.routerActive.snapshot.paramMap.get('id');
 
-    if (eventoId != null) {
+    if (this.eventoId != null || this.eventoId === 0) {
       this.metodo = 'put';
       this.novo = false;
       this.spinner.show();
 
-      this.eventoService.getEventoById(+eventoId).subscribe(
+      this.eventoService.getEventoById(this.eventoId).subscribe(
         (evento: Evento) => {
           this.evento = { ...evento };
           this.form.patchValue(this.evento);
+          this.evento.lotes.forEach(lote => {
+            this.novoLote = lote !== null;
+            this.lotes.push(this.criarLote(lote));
+          });
         },
         (erro: any) => {
           console.error(erro);
@@ -107,11 +128,14 @@ export class EventoDetalheComponent implements OnInit {
     });
   }
 
-  public resetarForm(): void {
+  public resetarFormEvento(): void {
     this.form.reset();
   }
 
-  public salvarAlteracao(evento: Evento): void {
+  public resetarFormLotes(): void {
+    this.form.controls.lotes.reset();
+  }
+  public salvarEvento(evento: Evento): void {
     this.spinner.show();
 
     if (this.form.valid) {
@@ -132,5 +156,50 @@ export class EventoDetalheComponent implements OnInit {
 
   public formValidator(campo: FormControl | AbstractControl): any {
     return { 'is-invalid': campo.errors && campo.touched };
+  }
+
+  public salvarLotes(): void {
+    this.spinner.show();
+    if (this.form.controls.lotes.valid){
+      this.loteService.saveLote(this.eventoId, this.form.value.lotes)
+        .subscribe(
+          () => {
+            this.toastr.success(this.novoLote ? 'Lotes cadastrados com sucesso' : 'Lotes alterados com sucesso');
+          },
+          (erro: any) => {
+            this.toastr.error(this.novoLote ? 'Erro ao cadastrar lotes' : 'Erro ao atualizar lotes', 'Atenção');
+          }
+        ).add(() => this.spinner.hide());
+    }
+  }
+
+  public removerLote(template: TemplateRef<any>, indice: number): void {
+    this.loteAtual.id = this.lotes.get(indice + '.id').value;
+    this.loteAtual.nome = this.lotes.get(indice + '.nome').value;
+    this.loteAtual.indice = indice;
+
+
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  public confirmDeleteLote(): void {
+    this.modalRef.hide();
+    this.spinner.show();
+
+    this.loteService.deleteLote(this.eventoId, this.loteAtual.id)
+      .subscribe(
+        () => {
+          this.toastr.success('Lote deletado com sucesso', 'Sucesso');
+          this.lotes.removeAt(this.loteAtual.indice);
+        },
+        (erro: any) => {
+          console.log(`Erro: ${erro}`);
+          this.toastr.error('Erro ao cadastrar lote', 'Atenção');
+        }
+      ).add(() => this.spinner.hide());
+  }
+
+  public declineDeleteLote(): void {
+    this.modalRef.hide();
   }
 }
--- Client/ProEventos-App/src/app/componentes/user/login/login.component.ts
+++ Client/ProEventos-App/src/app/componentes/user/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ActivatedRoute, Router } from '@angular/router';
+import {Login} from '@app/models/Login';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  login = {} as Login;
+  form: FormGroup = this.formBuilder.group({});
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.validacao();
   }
 
+  get l(): any {
+    return this.form.controls;
+  }
+
+  public formValidator(campo: FormControl | AbstractControl): any {
+    return { 'is-invalid': campo.errors && campo.touched };
+  }
+
+  public validacao(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(30)]]
+    });
+  }
+  public EntrarSistema(login: Login): void {
+
+  }
 }
